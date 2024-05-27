9eaab907db01a704367a0bc83fbd4dc88baed6ee
Wasif Mohammad <40202234+Waasif23@users.noreply.github.com>
2024-05-27 17:21:53 +0000

    Varibles Let, Const, and Varki Kahani.

---
+++ .devcontainer/01_Basics/01_variables.js
@@ -0,0 +1,7 @@
+const accID= 2313582;
+var accEmail= "m.wasif@tcs.com";
+let accPassword= "Wasif0902";
+ accCity="Nanded";
+ let accState;
+
+ console.table([accID, accEmail, accPassword, accCity, accState]);
