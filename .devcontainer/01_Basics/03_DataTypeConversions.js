9fbc0e63f887e836352a15d22d40d15a0ed5efc8
Wasif Mohammad <40202234+Waasif23@users.noreply.github.com>
2024-05-28 18:01:11 +0000

    conversions of DT

---
+++ .devcontainer/01_Basics/03_DataTypeConversions.js
@@ -0,0 +1,16 @@
+let varNum= 37;
+let varString ="37abc";
+let varBoolean = true;
+
+console.log(varNum + " is a value of type " + typeof(varNum));
+console.log(varBoolean + " is a value of type " + typeof(varBoolean));
+console.log(varString + " is a value of type " + typeof(varString));
+
+let  varStringCasting = String(varNum);
+let varNubmerCasting = (Number(varBoolean));
+let varBooleanCasting = Boolean(varBoolean);
+
+console.log(varStringCasting);
+console.log(varNubmerCasting);
+console.log(varBooleanCasting);
+
