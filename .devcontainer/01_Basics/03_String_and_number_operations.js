f93ff0ef61ef9d101ddb187c5abf3b9e8beffef4
Wasif Mohammad <40202234+Waasif23@users.noreply.github.com>
2024-05-29 06:58:22 +0000

    String and number conversion concept

---
+++ .devcontainer/01_Basics/03_String_and_number_operations.js
@@ -0,0 +1,16 @@
+let varValue =3;
+varValue=-varValue;
+// console.log(varValue);
+
+// console.log(3 + "2");
+// console.log("3" + "2");
+// console.log("3" + 2 + 2); // left to tight first type it will convert the entire statement
+// console.log(3 + 2 + "2");  // left to tight first type it will convert the entire statement
+
+let score=1;
+// score++;
+// console.log(score);
+
+++score;
+console.log(score);
+// Note: refer the prefix and postfix in JS MDN docs.
